1048b70889c8c4f37c3419edcb895f4a39cab3c1
AnhHoangQuach <hoanganh36.work@gmail.com>
2021-10-07 01:47:35 +0700

    feat: add function deposit token

--- defi-staking-app-starter/src/contracts/DecentralBank.sol
+++ defi-staking-app-starter/src/contracts/DecentralBank.sol
@@ -10,8 +10,27 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+    }
+
+    function depositTokens(uint256 _amount) public {
+        require(_amount > 0, "Amount cannot be 0");
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        stakingBalance[msg.sender] += _amount;
+
+        if (!hasStaked) {
+            stakers.push(msg.sender);
+        }
+        hasStaked[msg.sender] = true;
+        isStaked[msg.sender] = true;
     }
 }
